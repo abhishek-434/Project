c054350b1b54415ee4ddd5908a0b939c56cf7fb4
abhishek-434 <abhishekkumar2882003@gmail.com>
2025-07-28 01:10:21 +0530

    in tour project(changes in packages css and add about html, css and js)

--- tour_travel/travel_app/urls.py
+++ tour_travel/travel_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('packages/', views.packages, name='packages'),
+    path('about/', views.about, name='about')
 ]
